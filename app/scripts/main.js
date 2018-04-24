5adad7110ec51ba67f2f34e56fd28cf160d0d097
Paul <paul.mcneiljr@gmail.com>
2018-04-24 01:57:47 -0400

    add values to table with jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,14 @@
 $(function(){
 
     // set up
-    var $tbody = $('tbody');
-    var $newItemFormInventory = $('#newItemForm2');
+     var $tbody = $('tbody');
+    var $newItemFormInventory = $('#new-item');
     var $desc = $('#desc');
     var $serNum = $('#serial-num');
     var $loc = $('#loc');
-    var $status = $('#status'); 
-    
-    function getCount(){
-        var numItems = $('tbody').length;
-        console.log(numItems);
-    }
-    
-    function addTdItem()
-    {
+    var $rfidnum = $('#rfidnum'); 
+ 
+    function addTdItem(){
         $tbody.append(
             
             '<tr>' 
@@ -24,10 +18,17 @@ $(function(){
             + '</td> '
             + '<td>' + $loc.val() 
             + '</td> '
-            + '<td>' + $status.val() 
+            + '<td>' + $rfidnum.val() 
             + '</td> '
             + '</tr>'
         );
+    }
+
+    function clearForm(){
+        $serNum.val('');
+        $desc.val('');
+        $loc.val('');
+        $rfidnum.val('') ;
     }
 
     $newItemFormInventory.on('submit', function(e){
@@ -36,59 +37,9 @@ $(function(){
         //add serial number to td element
         // append element to new tr element
         addTdItem();
-        console.log($serNum.val());
-        console.log($desc.val())
-       
-        console.log($loc.val())
-        console.log($status.val()); 
-        
-        getCount();
-
+        clearForm();
     });
-
-
 });
-
-// function submit(){
-
-//     var serialInput = getElementById('serial-num').value;
-//     var descriptionInput = getElementById('description').value;
-//     var locationInput = getElementById('location').value;
-//     var statusInput = getElementById('status').value;
-
-//     console.log(serialInput);
-//     console.log(descriptionInput);
-//     console.log(locationInput);
-//     console.log(statusInput);
-
-    //createRow();
-    //createTD();
-    //appendTR();
-
-
-    // create a new element and store it in a variable
-    // var tableRow = document.createElement('tr');
-
-    // var serialNumTD = document.createElement('td');
-    // var descriptionTD = document.createElement('td');
-    // var locationTD = document.createElement('td');
-    // var statusTD = document.createElement('td');
-
-
-
-    // create a text node and store it in a variable
-    // var newText = document.createTextNode('new node');
-
-    // attach the new text node to the new element
-    // newEl.appendChild(newText);
-
-    // find the position where the new element should be added
-    // var position = document.getElementsByTagName('tbody')[0];
-
-    // insert the new element into its position
-    //position.appendChild(newEl);
-// }
-
 
 
 
